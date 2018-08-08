21cea2bcd7095fafe9303472b0f95ee00c5c91f5
Fabian Schuiki <fabian@schuiki.ch>
2018-08-08 16:41:26 +0200

    map parsed desc to grammar
    Extend the tool to map a parsed grammar description to the actual
    underlying grammar data structure. Also offer the possibility to dump
    the AST and the computed item sets to stdout. Generate the associated
    state machine, but don't produce any output code. This will help detect
    any conflicts in the grammar for now.

--- src/tool.rs
+++ src/tool.rs
@@ -5,12 +5,13 @@ extern crate memmap;
 extern crate perplex;
 
 use std::fs::File;
+use std::collections::HashMap;
 
 use clap::{App, Arg};
 use memmap::Mmap;
-// use perplex::grammar::{Grammar, Rule};
-// use perplex::item_set::ItemSets;
-// use perplex::machine::StateMachine;
+use perplex::grammar::{Grammar, NonterminalId, Rule, Symbol, TerminalId};
+use perplex::item_set::ItemSets;
+use perplex::machine::StateMachine;
 // use perplex::backend::{generate_parser, Backend};
 use perplex::lexer::Lexer;
 use perplex::parser::parse_iter;
@@ -26,6 +27,16 @@ fn main() {
                 .required(true)
                 .index(1),
         )
+        .arg(
+            Arg::with_name("dump_ast")
+                .long("dump-ast")
+                .help("Dump the input grammar AST and exit"),
+        )
+        .arg(
+            Arg::with_name("dump_sets")
+                .long("dump-sets")
+                .help("Dump the generated item sets and exit"),
+        )
         .get_matches();
 
     // Parse the input grammar description.
@@ -37,5 +48,59 @@ fn main() {
         let lex = Lexer::new(text.char_indices());
         parse_iter(lex.map(|(_, _, t)| t))
     };
-    println!("{:#?}", desc);
+
+    if matches.is_present("dump_ast") {
+        println!("{:#?}", desc);
+        return;
+    }
+
+    // Create a grammar and backend description from the parsed AST.
+    let mut grammar = Grammar::new();
+    // let mut backend = Backend::new();
+    {
+        // Declare the terminals and nonterminals.
+        let mut token_map: HashMap<String, TerminalId> = HashMap::new();
+        let mut rule_map: HashMap<String, NonterminalId> = HashMap::new();
+        for d in &desc.tokens {
+            token_map.insert(d.name.clone(), grammar.add_terminal(d.name.clone()));
+        }
+        for d in &desc.rules {
+            rule_map.insert(d.name.clone(), grammar.add_nonterminal(d.name.clone()));
+        }
+
+        // Create a unified symbol lookup table.
+        let mut symbol_map: HashMap<String, Symbol> = HashMap::new();
+        for (n, &i) in &token_map {
+            symbol_map.insert(n.clone(), i.into());
+        }
+        for (n, &i) in &rule_map {
+            if symbol_map.insert(n.clone(), i.into()).is_some() {
+                panic!("rule name `{}` conflicts with token name `{}`", n, n);
+            }
+        }
+
+        // Add the rules to the grammar.
+        for d in &desc.rules {
+            let id = rule_map[&d.name];
+            for v in &d.variants {
+                let seq = v.iter()
+                    .map(|v| match symbol_map.get(v) {
+                        Some(&s) => s,
+                        None => panic!("unknown token or rule `{}`", v),
+                    })
+                    .collect();
+                grammar.add_rule(Rule::new(id, seq));
+            }
+        }
+    }
+
+    // Determine the item sets.
+    let is = ItemSets::compute(&grammar);
+    if matches.is_present("dump_sets") {
+        println!("{}", is.pretty(&grammar));
+        return;
+    }
+
+    // Generate the associated state machine.
+    let _sm = StateMachine::try_from(&is).expect("failed to generate state machine");
 }
