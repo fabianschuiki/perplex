654b2492bd3ad9ec5fcc24588f3660993424b942
Fabian Schuiki <fabian@schuiki.ch>
2018-08-09 15:30:34 +0200

    impl conflict arc computation
    Implement computation of a conflict arc. This is done by iteratively
    stepping through the state space of the parser, spawning new ranks of
    conflict arc nodes. Iteration stops when no more ambiguities are present
    in the arc.

--- src/glr.rs
+++ src/glr.rs
@@ -2,11 +2,12 @@
 
 //! Analysis of the GLR/LR(k) behaviour of a grammar.
 
+use std::fmt;
 use std::ops::{Index, IndexMut};
-use std::collections::{BTreeSet, HashSet, VecDeque};
+use std::collections::{HashSet, VecDeque};
 use indexmap::{IndexMap, IndexSet};
 
-use grammar::{Grammar, Symbol};
+use grammar::{Grammar, Symbol, TerminalId};
 use item_set::{Action, ItemSetId, ItemSets};
 
 /// An analysis of conflicts and their resolvability via GLR/LR(k).
@@ -18,7 +19,8 @@ impl GlrAnalysis {
     pub fn compute(grammar: &Grammar, item_sets: &ItemSets) -> GlrAnalysis {
         let conflicts = find_conflicts(item_sets);
         for conflict in conflicts {
-            find_conflict_arc(&conflict, grammar, item_sets);
+            let arc = find_conflict_arc(&conflict, grammar, item_sets);
+            println!("{:?}", arc);
         }
         GlrAnalysis {}
     }
@@ -68,7 +70,11 @@ pub struct Conflict {
 }
 
 /// Find the arc of a conflict.
-pub fn find_conflict_arc(conflict: &Conflict, grammar: &Grammar, item_sets: &ItemSets) {
+pub fn find_conflict_arc(
+    conflict: &Conflict,
+    grammar: &Grammar,
+    item_sets: &ItemSets,
+) -> ConflictArc {
     println!("analyzing {:#?}", conflict);
     // Initialize the conflict arc with one root node. Then advance that node
     // such that each item set it contains is at a location where a shift may
@@ -79,12 +85,30 @@ pub fn find_conflict_arc(conflict: &Conflict, grammar: &Grammar, item_sets: &Ite
                 rank: 0,
                 item_sets: vec![conflict.item_set],
                 parents: vec![],
+                shifts: vec![],
+                resolved: false,
             },
         ],
-        ranks: vec![vec![0]],
+        ranks: vec![vec![ConflictNodeId(0)]],
     };
     advance_node_to_shift(&mut arc, ConflictNodeId(0), grammar, item_sets);
-    println!("{:#?}", arc);
+
+    // Iteratively advance the arc through the state space until all ambiguities
+    // have been resolved (i.e. when no more ranks are spawned).
+    for i in 0.. {
+        if i > 1000 {
+            panic!("conflict arc analysis did not converge after 1000 iterations");
+        }
+        let any_spawned = spawn_next_rank(&mut arc, grammar, item_sets);
+        if !any_spawned {
+            break;
+        }
+        for node_id in arc.ranks.last().unwrap().clone() {
+            advance_node_to_shift(&mut arc, node_id, grammar, item_sets);
+        }
+    }
+
+    arc
 }
 
 /// Advance the item sets of a node to points in the graph where a shift action
@@ -140,7 +164,7 @@ fn advance_node_to_shift(
                     }
 
                     // Step back `len` nodes.
-                    let mut nodes = BTreeSet::new();
+                    let mut nodes = IndexSet::new();
                     nodes.insert(node);
                     for _ in 0..len {
                         nodes = nodes
@@ -189,15 +213,97 @@ fn advance_node_to_shift(
     arc[node].item_sets = new_item_sets;
 }
 
+/// Trace through all shift actions in the last rank of the arc and thus spawn
+/// the next rank of nodes. Returns true if any nodes were spawned, or false if
+/// all conflicts were resolved.
+fn spawn_next_rank(arc: &mut ConflictArc, grammar: &Grammar, item_sets: &ItemSets) -> bool {
+    let new_rank = arc.ranks.len();
+    let mut new_rank_nodes = vec![];
+
+    for node_id in arc.ranks.last().unwrap().clone() {
+        println!("expanding {}", node_id);
+
+        // Determine the item sets that are destinations of shift actions. Group
+        // these sets by the terminal that causes the shift. This then indicates
+        // if there is any ambiguity involved in reading a terminal.
+        let mut shifts: IndexMap<TerminalId, IndexSet<ItemSetId>> = IndexMap::new();
+
+        for &is_id in arc[node_id].item_sets.iter() {
+            println!(" - tracing from {} i{}", node_id, is_id);
+            for &(symbol, action) in item_sets[is_id].actions() {
+                if let (Symbol::Terminal(t), Action::Shift(target)) = (symbol, action) {
+                    println!(" - {} -> i{}", t.pretty(grammar), target);
+                    shifts.entry(t).or_insert_with(IndexSet::new).insert(target);
+                }
+            }
+        }
+
+        // Create new nodes in the conflict arc for each shift that causes
+        // an ambiguity as indicated by multiple target item sets. All other
+        // shifts indicate a resolution of ambiguity.
+        println!(" - shifts from {} {:?}", node_id, shifts);
+        let mut all_resolved = true;
+        for (terminal, mut item_sets) in shifts {
+            if item_sets.len() > 1 {
+                println!(
+                    " - shift {} has item sets {:?}",
+                    terminal.pretty(grammar),
+                    item_sets
+                );
+                let new_node_id = ConflictNodeId(arc.nodes.len());
+                let new_node = ConflictNode {
+                    rank: new_rank,
+                    item_sets: item_sets.into_iter().collect(),
+                    parents: vec![node_id],
+                    shifts: vec![],
+                    resolved: false,
+                };
+                arc.nodes.push(new_node);
+                new_rank_nodes.push(new_node_id);
+                arc[node_id]
+                    .shifts
+                    .push((terminal, ConflictEdge::Ambiguous(new_node_id)));
+                all_resolved = false;
+            } else {
+                println!(" - shift {} resolves ambiguity", terminal.pretty(grammar));
+                arc[node_id]
+                    .shifts
+                    .push((terminal, ConflictEdge::Resolved(item_sets.pop().unwrap())));
+            }
+        }
+        arc[node_id].resolved = all_resolved;
+    }
+
+    if new_rank_nodes.is_empty() {
+        false
+    } else {
+        arc.ranks.push(new_rank_nodes);
+        true
+    }
+}
+
 /// A subspace of a parser's state space within which a conflict is active.
 #[derive(Debug)]
 pub struct ConflictArc {
     nodes: Vec<ConflictNode>,
-    ranks: Vec<Vec<usize>>,
+    ranks: Vec<Vec<ConflictNodeId>>,
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
-struct ConflictNodeId(usize);
+/// A unique identifier for a node in a conflict arc.
+#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct ConflictNodeId(usize);
+
+impl fmt::Display for ConflictNodeId {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "n{}", self.0)
+    }
+}
+
+impl fmt::Debug for ConflictNodeId {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self)
+    }
+}
 
 impl Index<ConflictNodeId> for ConflictArc {
     type Output = ConflictNode;
@@ -219,4 +325,14 @@ pub struct ConflictNode {
     rank: usize,
     item_sets: Vec<ItemSetId>,
     parents: Vec<ConflictNodeId>,
+    shifts: Vec<(TerminalId, ConflictEdge)>,
+    resolved: bool,
+}
+
+/// An edge from one conflict node to another.
+#[allow(missing_docs)]
+#[derive(Debug)]
+pub enum ConflictEdge {
+    Resolved(ItemSetId),
+    Ambiguous(ConflictNodeId),
 }
